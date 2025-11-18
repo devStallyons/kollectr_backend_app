5eafcb048b684e2c71e1ac4f270435fc1c4ddda4
stallyons-developer1 <dev.stallyons@gmail.com>
2025-11-18 18:55:56 +0500

    cors correct

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(corsMiddleware);
-// app.use(validateOrigin);
+app.use(validateOrigin);
 //rate limit
 app.use(limiter);
 //helmet
